a9d4d5c32eec2508389a2919c44ed4f3db2faf4d
cendikiaapermataa <cendikiapermata@gmail.com>
2026-03-12 11:25:47 +0700

    pertama commit

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -11,3 +11,10 @@ User user1 = User(
   password: '011',
   nama: 'Cendikia Permata Dewanti',
 );
+
+// Di dalam user.dart
+// List<User> daftarUser = [
+  //User(username: 'cece', password: '011', nama: 'Cendikia Permata'),
+  //User(username: 'admin', password: '123', nama: 'Admin Toko'),
+  //User(username: 'dosen', password: '999', nama: 'Bapak Dosen'),
+//];
